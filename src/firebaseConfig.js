673d2e70d4169c7e78fd806892e358b09a851f27
tanjim <tanjimislamh@gmail.com>
2020-10-13 13:11:52 +0600

    finished

---
+++ src/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAEBcBq1Hzjqh-qSq-0HO9ayis240kRBHw",
+  authDomain: "ema-jhon-fbf34.firebaseapp.com",
+  databaseURL: "https://ema-jhon-fbf34.firebaseio.com",
+  projectId: "ema-jhon-fbf34",
+  storageBucket: "ema-jhon-fbf34.appspot.com",
+  messagingSenderId: "580595883154",
+  appId: "1:580595883154:web:4860d4069aef3442739a4b"
+};
+export default firebaseConfig;
